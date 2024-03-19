e3466504484e46920d0a6251bf272763fe940319
Anish Goel <anishgoel1129@gmail.com>
2024-03-19 10:19:12 +0530

    Add files via upload

---
+++ 83.c
@@ -0,0 +1,13 @@
+struct ListNode* deleteDuplicates(struct ListNode* head){
+    if(head==NULL || head->next == NULL)return head;
+    struct ListNode *temp=head->next,*lastValidElement=head;
+    while(temp!=NULL){
+        if(temp->val != lastValidElement->val){
+            lastValidElement->next=temp;
+            lastValidElement=temp;
+        }
+        temp=temp->next;
+    }
+    lastValidElement->next=temp;
+    return head;
+}
